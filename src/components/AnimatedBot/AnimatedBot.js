bd65a6092f06b13efceeacb4e8f35d46312ff2bc
kiwizu3 <kavinda@sqrmobile.com>
2021-09-29 16:56:19 +0530

    deleted old files

--- src/components/AnimatedBot/AnimatedBot.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './AnimatedBot.css';
-
-const AnimatedBot = () => {
-    return(
-        <img className="img-fluid" src="./assets/images/bota-temp.png" alt="bot-animation-here" />
-    )
-}
-export default AnimatedBot;
